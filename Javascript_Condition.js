a78301e7c75a32fcbbc1cf506e74d21ce07cfb8d
Sainzel <cppnaf@gmail.com>
2026-05-15 22:31:06 +0700

    Condition

---
+++ Javascript_Condition.js
@@ -0,0 +1,16 @@
+let age = 16;
+let text = "You can Not drive";
+
+if (age >= 18) {
+  text = "You can drive";
+}
+
+let age = 16;
+let country = "USA";
+let text = "You can Not drive!";
+
+if (country == "USA") {
+  if (age >= 16) {
+    text = "You can drive!";
+  }
+}
